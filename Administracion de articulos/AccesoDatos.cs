263b63217a7e05f290db4109ea80d959d3c00e0d
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-03-19 07:42:37 -0300

    Agregados archivos iniciales
    Archivos que permiten el funcionamiento del proyecto, este commit sera el mas grande, aca se creo una clase de Acceso a datos que permite la conexion de la aplicacion con la base de datos usando ADO.NET

---
+++ Administracion de articulos/AccesoDatos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administracion_de_articulos
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+        public void setConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                comando.Connection.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+
+
+        }
+    }
+}

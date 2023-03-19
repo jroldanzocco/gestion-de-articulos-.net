571642d68d3d8849861204851423aebeb39e4327
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-03-19 08:21:50 -0300

    Muestra de base de datos en pantalla
    Se Creo una lista de articulos a la que se le asignaron objetos de tipo articulo con columnas necesarias para mostrar cada dato que envia la BD

--- Administracion de articulos/Form1.cs
+++ Administracion de articulos/Form1.cs
@@ -20,7 +20,25 @@ namespace Administracion_de_articulos
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            List<Articulo> listaArticulos = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+            datos.setConsulta("SELECT A.Id, A.Codigo, A.Nombre, A.Descripcion, M.Descripcion Marca, C.Descripcion Categoria, A.ImagenUrl, A.Precio  FROM ARTICULOS A INNER JOIN MARCAS M ON A.IdMarca = M.Id INNER JOIN CATEGORIAS C ON A.IdCategoria = C.Id");
+            datos.ejecutarLectura();
+            while (datos.Lector.Read())
+            {
+                Articulo articulo= new Articulo();
+                articulo.Id = (int)datos.Lector["Id"];
+                articulo.Codigo = (string)datos.Lector["Codigo"];
+                articulo.Nombre = (string)datos.Lector["Nombre"];
+                articulo.Descripcion = (string)datos.Lector["Descripcion"];
+                articulo.Marca = (string)datos.Lector["Marca"];
+                articulo.Categoria = (string)datos.Lector["Categoria"];
+                articulo.UrlImagen = (string)datos.Lector["ImagenUrl"];
+                articulo.Precio = Convert.ToSingle(datos.Lector["Precio"]);
+
+                listaArticulos.Add(articulo);
+            }
+            dgvArticulos.DataSource = listaArticulos;
         }
     }
 }
